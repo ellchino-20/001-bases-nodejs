7488073c513dc40535e9778cb627ee9d2ccfe5bb
Emilio.Jose <emilio.34711@gmail.com>
2021-02-18 22:22:01 -0600

    commit 2

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
-const multplicar = require('./Multiplicacion/multiplicacion');
+const argv = require('yargs')
+    .command('listar', 'Imprime en pantalla la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            default: 10,
+            alias: 'l'
+        }
+    })
+    .command('crear', 'Crea un archivo con la tabla de multimplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            default: 10,
+            alias: 'l'
+        }
+    })
+    .argv;
+const { alias } = require('yargs');
+const multiplicar = require('./multiplicacion/multiplicacion');
 
 
-multplicar
+let comando = argv._[0];
+let base = argv.base;
+let limite = argv.limite;
+switch (comando) {
+    case 'listar':
 
-.crearArchivo(3)
-.then(archivo => console.log(`El archivo ${archivo} ha sido generado con exito`))
-.catch(error => console.log(`Ocurrio un error ${archivo} `));
+
+
+        multiplicar
+            .listarTabla(base, limite)
+            .then()
+            .catch(error => console.log(`Ocurrio un error ${error}`));
+        break;
+    case 'crear':
+        multiplicar
+            .crearArchivo(base, limite)
+            .then(archivo => console.log(`El archivo ${archivo} ha sido generado con exito`))
+            .catch(error => console.log(`Ocurrio un error ${error}`));
+        break;
+    default:
+        console.log('Comando no reconocido')
+}
